1f5b49ab2361ae1344dccfbdc1276a51f3a9fb24
Lucas Evangelista <lucas.silva@sptech.school>
2023-06-19 18:13:05 -0300

    Extructure of shop api

---
+++ shop-api/src/test/java/com/springkafka/shopapi/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springkafka.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
